21759dcb683949998fc7ff7573b93ead1842ed8f
NatalyaDemiyanova <natalya.demiyanova@gmail.com>
2020-07-24 14:54:49 +0300

    deploy

---
+++ js/slider-news.js
@@ -0,0 +1,3 @@
+!function(t){"use strict";new(t=t&&Object.prototype.hasOwnProperty.call(t,"default")?t.default:t)(".glide").mount()}(Glide);
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2xpZGVyLW5ld3MuanMiLCJzb3VyY2VzIjpbInNyYy9qcy9zbGlkZXItbmV3cy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgR2xpZGUgZnJvbSAnQGdsaWRlanMvZ2xpZGUnXHJcblxyXG5uZXcgR2xpZGUoJy5nbGlkZScpLm1vdW50KCkiXSwibmFtZXMiOlsibW91bnQiXSwibWFwcGluZ3MiOiIwQkFFQSx3RUFBVSxVQUFVQSJ9
